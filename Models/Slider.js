041c16f1428c8245305bfa3d4623701a2d0d5fc5
Naveed <mastermindgeh@gmail.com>
2022-09-22 01:20:17 +0500

    thrid push

--- Models/Slider.js
+++ Models/Slider.js
@@ -1,9 +1,12 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const SliderSchema=new mongoose.Schema({
-    slierCaption:{type:String},
-    sliderImage:{type:String},
-    
-},{timestamps:true});
-  const Slider=mongoose.model("Slider",SliderSchema);
-  module.exports=Slider;
+const SliderSchema = new mongoose.Schema(
+  {
+    slierCaption: { type: String },
+    sliderImage: { type: String },
+    sliderurl: { type: String },
+  },
+  { timestamps: true }
+);
+const Slider = mongoose.model("Slider", SliderSchema);
+module.exports = Slider;
